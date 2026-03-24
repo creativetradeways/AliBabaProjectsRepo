d009796788b19bc3917ffb14bd15af885661f2cc
Omar Sadiq <osadiq@neomtech.com>
2026-03-24 17:43:12 +0300

    commit 2

--- Project1_withAliBaba/Controllers/WeatherForecastController.cs
+++ Project1_withAliBaba/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Project1_withAliBaba.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Thunderstrom"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Thunderstrom", "Thunderstrom2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
